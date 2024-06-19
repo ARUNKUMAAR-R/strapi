df63cce995d52c64a4ee5b1baa8f610640848a57
Albatroz <118113700+ARUNKUMAAR-R@users.noreply.github.com>
2024-06-19 20:31:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -35,22 +35,22 @@ resource "aws_instance" "strapi-ec2" {
   key_name                    = "NV-PPK"
   associate_public_ip_address = true
   user_data                   = <<-EOF
-    #!/bin/bash
-
-    sudo apt update
-    curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-    sudo bash -E nodesource_setup.sh
-    sudo apt update && sudo apt install nodejs -y
-    sudo npm install -g yarn && sudo npm install -g pm2
-    echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
-    cd simple-strapi
-    echo "const strapi = require('@strapi/strapi');
-    strapi().start();" > server.js
-    pm2 start server.js
-    EOF
+                                #!/bin/bash
+                                sudo apt update
+                                curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+                                sudo bash -E nodesource_setup.sh
+                                sudo apt update && sudo apt install nodejs -y
+                                sudo npm install -g yarn && sudo npm install -g pm2
+                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
+                                cd simple-strapi
+                                echo "const strapi = require('@strapi/strapi');
+                                strapi().start();" > server.js
+                                pm2 start server.js
+                                sleep 320
+                                EOF
 
   tags = {
-    Name = "Strapi_ec2"
+    Name = "Strapi_ec2_automation"
   }
 }
 
